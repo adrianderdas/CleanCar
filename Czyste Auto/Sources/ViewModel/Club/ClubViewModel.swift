de349dbfc7622565d98d37d5800fcdab3df4cd1b
adrianderdas <derdas.adrian@gmail.com>
2023-10-08 15:33:49 +0200

    Reorganize code

---
+++ Czyste Auto/Sources/ViewModel/Club/ClubViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ClubViewModel.swift
+//  Czyste Auto
+//
+//  Created by Adrian Derdaś on 08/10/2023.
+//
+
+import Foundation
---
+++ Czyste Auto/Sources/ViewModel/Summary/OrdersViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrdersViewModel.swift
+//  Czyste Auto
+//
+//  Created by Adrian Derdaś on 08/10/2023.
+//
+
+import Foundation
