96e4e2db4fc6b38382c1bd95010469b078eead45
adrianderdas <derdas.adrian@gmail.com>
2024-03-30 21:56:13 +0100

    UIView instead of a UIButton for footerView
    Improved code readability, used an intermediary UIView instead of a Button as the footerView. Replaced some Framebased layout with Autolayout

--- Czyste Auto/Scenes/Settings/ViewController/SettingsViewController.swift
+++ Czyste Auto/Scenes/Settings/ViewController/SettingsViewController.swift
@@ -1,54 +1,41 @@
 import UIKit
 import FirebaseFirestore
-//import JGProgressHUD
 import FirebaseAuth
 
 class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
- //   private let spinner = JGProgressHUD(style: .dark)
     var userID = FirebaseService.shared.getuserId()
-    
     var firstName: String = FirebaseService.shared.firebaseFirstName ?? "Brak danych"
     var lastName: String = FirebaseService.shared.firebaseLastName ?? "Brak danych"
     var phone: String = FirebaseService.shared.firebasePhone ?? "Brak danych"
     var email: String = FirebaseService.shared.firebaseEmail ?? "Brak danych"
-    
-   
-    
     var city: String = FirebaseService.shared.firebaseCity ?? "Brak danych"
     var postalCode: String = FirebaseService.shared.firebasePostalCode ?? "Brak danych"
     var houseNumber: String = FirebaseService.shared.firebaseHouseNumber ?? "Brak danych"
 
-   
-    
     
     private let logOutButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Wyloguj się", for: .normal)
         button.backgroundColor = .red
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 12
         button.layer.masksToBounds = true
         button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        
         return button
     }()
     
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.clipsToBounds = true
-        return scrollView
-    }()
-    
-    private let contentContainerView: UIView = {
+    private let footerView: UIView = {
         let view = UIView()
+  
         return view
     }()
     
-    
-  
+ 
     private var tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
+        table.translatesAutoresizingMaskIntoConstraints = false
         table.allowsSelection = false
         table.register(CustomCellForOrders.self, forCellReuseIdentifier: "cell")
         return table
@@ -56,8 +43,6 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     
     @objc private func didTapLogOut() {
        
-        
-        
         let actionSheet = UIAlertController(title: "Wylogowywanie", message: "Czy na pewno chcesz się wylogować?", preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "Wyloguj się", style: .destructive, handler: { [weak self] _ in
             guard let strongSelf = self else {
@@ -79,59 +64,56 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         }))
                 
         actionSheet.addAction(UIAlertAction(title: "Anuluj", style: .cancel, handler: nil))
-        
         self.present(actionSheet, animated: true)
-
-        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //view.backgroundColor = .systemBackground
-        
-        title = "Ustawienia"
-        
-        navigationItem.largeTitleDisplayMode = .never
-        navigationController?.navigationBar.isTranslucent = false
-        
-        logOutButton.addTarget(self, action: #selector(didTapLogOut), for: .touchUpInside)
-        
-        
+        setView()
+        setConstraints()
+        setTableView()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        let buttonHeight: CGFloat = 50
-    
-       
-        tableView.frame = CGRect(x: 0, y: 0, width: view.width, height: view.height)
-        
-        logOutButton.frame = CGRect(x: 0, y: tableView.bottom+buttonHeight+60, width: tableView.frame.width, height: buttonHeight)
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            logOutButton.leadingAnchor.constraint(equalTo: footerView.layoutMarginsGuide.leadingAnchor),
+            logOutButton.topAnchor.constraint(equalTo: footerView.topAnchor),
+            logOutButton.trailingAnchor.constraint(equalTo: footerView.layoutMarginsGuide.trailingAnchor),
+            logOutButton.bottomAnchor.constraint(equalTo: footerView.bottomAnchor),
+        ])
     }
-     
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-               
-        view.addSubview(tableView)
-        
+    func setTableView() {
         tableView.dataSource = self
         tableView.rowHeight = 80
         tableView.delegate = self
-        tableView.tableFooterView = logOutButton
-        
-        
-        
+        tableView.tableFooterView = footerView
+    }
+    
+    func setView() {
+        title = "Ustawienia"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.isTranslucent = false
+        logOutButton.addTarget(self, action: #selector(didTapLogOut), for: .touchUpInside)
+        view.addSubview(tableView)
+        footerView.addSubview(logOutButton)
+        let buttonHeight: CGFloat = 50
+        footerView.frame = CGRect(x: 0, y: 0, width: 0, height: buttonHeight)
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: 60))
-        
-        
+    
         if section  == 0 {
             let label = UILabel()
             let imageView = UIImageView(image: UIImage(systemName: "person"))
@@ -151,27 +133,20 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             let imageView = UIImageView(image: UIImage(systemName: "house"))
             imageView.tintColor = UIColor.label
             imageView.contentMode = .scaleAspectFit
-            
-            //let imageEditView = UIImageView(image: UIImage(systemName: "square.and.pencil"))
-            
-            
+                        
             let label = UILabel()
             
             header.addSubview(imageView)
             
             imageView.frame = CGRect(x: 5, y: 5, width: 50, height: 50)
             label.frame = CGRect(x: imageView.frame.width+10, y: 5, width: 200, height: 50)
-           // imageEditView.frame = CGRect(x: view.width-60, y: 5, width: 50, height: 50)
-            
+    
             
             header.addSubview(label)
             label.text = "Adres odbioru pojazdu"
-            //header.addSubview(imageEditView)
             return header
             
         }
-
-      
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -234,14 +209,4 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             return cell
         }
     }
-    
-    
 }
-
-
-
-
-
-
-
-
